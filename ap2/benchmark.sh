e47f2b9394e31f033e44348f1bfdefa7d84971b7
Gabriel de Souza Seibel <gabrielseibel1@gmail.com>
2019-06-08 13:04:32 -0300

    Have more robust script that runs benchmark.

--- ap2/benchmark.sh
+++ ap2/benchmark.sh
@@ -7,17 +7,17 @@ cmake ..
 make
 
 #run
-echo "\n\n ./pi 8 100"
+echo "\n\n./pi 8 100"
 ./pi 8 100
 
-echo "\n\n ./mm 500 8 100"
+echo "\n\n./mm 500 8 100"
 ./mm 500 8 100
 
-echo "\n\n ./mm 750 8 100"
+echo "\n\n./mm 750 8 100"
 ./mm 750 8 100
 
-echo "\n\n ./mm 1000 8 100"
+echo "\n\n./mm 1000 8 100"
 ./mm 1000 8 100
 
-echo "\n\n ./mm 2000 8 100"
+echo "\n\n./mm 2000 8 100"
 ./mm 2000 8 100
