8e8f94f8b83928c4a9f4d85a8f0e7be1133e3cec
Gabriel de Souza Seibel <gabrielseibel1@gmail.com>
2019-06-08 13:51:38 -0300

    Have fewer terms to average (takes less time).

--- ap2/benchmark.sh
+++ ap2/benchmark.sh
@@ -10,17 +10,17 @@ make
 echo "\n\nCPU info:\n"
 sudo lshw -class cpu
 
-echo "\n\n./pi 8 100"
-./pi 8 100
+echo "\n\n./pi 8 10"
+./pi 8 10
 
-echo "\n\n./mm 500 8 100"
-./mm 500 8 100
+echo "\n\n./mm 500 8 10"
+./mm 500 8 10
 
-echo "\n\n./mm 750 8 100"
-./mm 750 8 100
+echo "\n\n./mm 750 8 10"
+./mm 750 8 10
 
-echo "\n\n./mm 1000 8 100"
-./mm 1000 8 100
+echo "\n\n./mm 1000 8 10"
+./mm 1000 8 10
 
-echo "\n\n./mm 2000 8 100"
-./mm 2000 8 100
+echo "\n\n./mm 2000 8 10"
+./mm 2000 8 10
